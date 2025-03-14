527df0a7ae0390bd4d3e1b60f48009873183e545
Zehui Zhang <zzha0455@student.monash.edu>
2025-03-14 14:13:08 +1100

    Reform file structure

--- my-health-app/src/router.js
+++ my-health-app/src/router.js
@@ -1,13 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/components/Home.vue";
-import UserDashboard from "@/components/UserDashboard.vue";
-import Login from "@/components/UserLogin.vue";
-import Register from "@/components/UserRegister.vue";
-import Blog from "@/components/Blog.vue";
-import MapboxMap from "@/components/MapboxMap.vue";
-import AppointmentCalendar from "@/components/AppointmentCalendar.vue";
-import CheckUV from "@/components/CheckUV.vue";
-import PersonalizedAdvice from "@/components/PersonalizedAdvice.vue";
+import Home from "@/views/Home.vue";
+import CheckUV from "@/views/CheckUV.vue";
+import PersonalizedAdvice from "@/views/PersonalizedAdvice.vue";
 
 const routes = [
   {
@@ -16,40 +10,9 @@ const routes = [
     component: Home,
   },
   {
-    path: "/check-uv", // <-- Add a new route for UV checking
+    path: "/check-uv",
     name: "CheckUV",
     component: CheckUV,
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register,
-  },
-  {
-    path: "/user-dashboard",
-    name: "UserDashboard",
-    component: UserDashboard,
-    meta: { requiresAuth: true, roles: ["User"] },
-  },
-  {
-    path: "/blog",
-    name: "Blog",
-    component: Blog,
-  },
-  {
-    path: "/map",
-    name: "Map",
-    component: MapboxMap,
-  },
-  {
-    path: "/appointment-calendar",
-    name: "AppointmentCalendar",
-    component: AppointmentCalendar,
   },
   {
     path: "/personalized-advice",
