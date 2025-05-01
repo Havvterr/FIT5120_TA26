7a586c73ad595c70bdcfdd2ab5c420ad87528340
rlia0025 <rlia0025@student.monash.edu>
2025-05-01 15:09:42 +1000

    Change api to preapere for deployment env

--- mel-eco-nest/src/services/aiDesignService.js
+++ mel-eco-nest/src/services/aiDesignService.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const API_URL = import.meta.env.VITE_API_URL || '/api'; //uncomment this line for deployment
+
 
 // Custom error class
 class AIDesignError extends Error {
